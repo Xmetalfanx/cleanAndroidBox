e9b11d527d8fc79d35df4811424cbbebac1034c2
Xmetalfanx <Xmetalfanx@yahoo.com>
2024-01-27 21:54:08 -0500

    f-droid code work

--- fdroid.sh
+++ fdroid.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
+# source: https://github.com/mvdan/fdroidcl
+
 rethink="com.celzero.bravedns"
 fdroid="org.fdroid.fdroid"
 aurora-store="com.aurora.store"
 super-freeze="superfreeze.tool.android"
 
+function detect-install-go() {
+    [ $(command -v go) ] && echo "Go support detected" && return || echo "Go not detected"
+
+    echo "Installing Go support"
+    [ $(command -v apt ) ] && sudo apt install golang
+    [ $(command -v dnf ) ] && sudo dnf -y install go
+    [ $(command -v eopkg ) ] && sudo eopkg install golang
+    [ $(command -v pacman ) ] && sudo pacman -S go
+    [ $(command -v zypper ) ] && sudo zypper install go
+
+}
+
+###################################################
+function install-fdroidcl() {
+    detect-install-go
+
+    go install mvdan.cc/fdroidcl@latest
+}
+
+
+install-fdroidcl
 
 function install-update() {
     echo -e "Installing or Updating ${1}"
-    fdroidcl install $2
+    fdroidcl install "${2}"
 }
 
 # Fdroid client 
